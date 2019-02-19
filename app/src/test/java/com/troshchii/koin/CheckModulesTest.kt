d208c10136d0c2e8b6b72076e188d2df7ab0378e
Yevhen Troshchii <yvgen.troshchiy@gmail.com>
2019-02-19 09:54:46 +0200

    Write CheckModulesTest

--- app/src/test/java/com/troshchii/koin/CheckModulesTest.kt
+++ app/src/test/java/com/troshchii/koin/CheckModulesTest.kt
@@ -1,10 +1,18 @@
 package com.troshchii.koin
 
+import org.junit.After
 import org.junit.Test
+import org.koin.standalone.StandAloneContext.stopKoin
 import org.koin.test.KoinTest
+import org.koin.test.checkModules
 
 class CheckModulesTest : KoinTest {
 
     @Test fun `check all modules`() {
+        checkModules(listOf(appModule))
+    }
+
+    @After fun close() {
+        stopKoin()
     }
 }
